3ce6dc481021088c475e8644087dba6330864310
Paul Tobin <tobster111@gmail.com>
2018-11-28 10:26:37 +0000

    Added Users database and Users schema

--- index.js
+++ index.js
@@ -2,6 +2,46 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const { buildSchema } = require('graphql');
 
+const db = {
+  users: [
+    {
+      id: '1',
+      email: 'alex@gmail.com',
+      name: 'Alex',
+      avatarUrl: 'https://gravatar.com/...'
+    },
+    {
+      id: '2',
+      email: 'bill@gmail.com',
+      name: 'Bill',
+      avatarUrl: 'https://gravatar.com/...'
+    },
+    {
+      id: '3',
+      email: 'jane@gmail.com',
+      name: 'Jane',
+      avatarUrl: 'https://gravatar.com/...'
+    }
+  ]
+};
+
+const schema = buildSchema(`
+  type Query {
+    users: [User!]!
+  }
+
+  type User {
+    id: ID!
+    email: String!
+    name: String
+    avatarUrl: String
+  }
+`);
+
+const rootValue = {
+  users: () => db.users
+}
+
 const app = express();
 
 app.use(
