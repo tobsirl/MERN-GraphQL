085f20c7863841e5a4f6165bfb6ce8c1d7342de4
Paul Tobin <tobster111@gmail.com>
2018-11-30 21:07:05 +0000

    Added Apollo Server

--- index.js
+++ index.js
@@ -81,4 +81,4 @@ const resolvers = {
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
-server.listen().then(({ url }) => console.log(url));
+server.listen().then(({ url }) => console.log(`Server running at ${url}`));
