9f8245dd1f82b38a2b3a64b51ad5ae833ab51f0c
Paul Tobin <tobster111@gmail.com>
2018-12-01 12:51:38 +0000

    Added Express + esm

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import { ApolloServer, gql } from 'apollo-server-express';
-import { typeDefs, resolvers } from './schema';
+import express from 'express';
+
+import typeDefs from './typeDefs';
+import resolvers from './resolvers';
+
+const app = express();
+
+app.disable('x-powered-by');
 
 const server = new ApolloServer({
   // These will be defined for both new or existing servers
   typeDefs,
-  resolvers,
+  resolvers
 });
 
 server.applyMiddleware({ app }); // app is from an existing express app
 
 app.listen({ port: 4000 }, () =>
   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
-)
+);
