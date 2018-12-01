7d612ec41d1f79d3dff57b4aad2a9aadc62973ce
Paul Tobin <tobster111@gmail.com>
2018-12-01 13:42:22 +0000

    Added dotenv + typeDefs/resolvers

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import express from 'express';
 import typeDefs from './typeDefs';
 import resolvers from './resolvers';
 
+require('dotenv').config();
+
 const app = express();
 
 app.disable('x-powered-by');
@@ -11,11 +13,14 @@ app.disable('x-powered-by');
 const server = new ApolloServer({
   // These will be defined for both new or existing servers
   typeDefs,
-  resolvers
+  resolvers,
+  playground: NODE_ENV !== 'production'
 });
 
 server.applyMiddleware({ app }); // app is from an existing express app
 
-app.listen({ port: 4000 }, () =>
-  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+app.listen({ port: PORT }, () =>
+  console.log(
+    `🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`
+  )
 );
