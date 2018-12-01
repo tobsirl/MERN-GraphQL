8877ad3155b2c1411883e0facf29acd835a97824
Paul Tobin <tobster111@gmail.com>
2018-12-01 11:20:31 +0000

    Changed to Apollo-Server-Express

--- index.js
+++ index.js
@@ -1,84 +1,84 @@
-const { ApolloServer, gql } = require('apollo-server');
-const crypto = require('crypto');
+// const { ApolloServer, gql } = require('apollo-server');
+// const crypto = require('crypto');
 
-const db = {
-  users: [
-    {
-      id: '1',
-      email: 'alex@gmail.com',
-      name: 'Alex',
-      avatarUrl: 'https://gravatar.com/...'
-    },
-    {
-      id: '2',
-      email: 'bill@gmail.com',
-      name: 'Bill',
-      avatarUrl: 'https://gravatar.com/...'
-    },
-    {
-      id: '3',
-      email: 'jane@gmail.com',
-      name: 'Jane',
-      avatarUrl: 'https://gravatar.com/...'
-    }
-  ],
-  messages: [
-    { id: '1', userId: '1', body: 'Hello', createdAt: Date.now() },
-    { id: '2', userId: '2', body: 'Hi', createdAt: Date.now() },
-    { id: '3', userId: '1', body: 'Hi Ho', createdAt: Date.now() }
-  ]
-};
+// const db = {
+//   users: [
+//     {
+//       id: '1',
+//       email: 'alex@gmail.com',
+//       name: 'Alex',
+//       avatarUrl: 'https://gravatar.com/...'
+//     },
+//     {
+//       id: '2',
+//       email: 'bill@gmail.com',
+//       name: 'Bill',
+//       avatarUrl: 'https://gravatar.com/...'
+//     },
+//     {
+//       id: '3',
+//       email: 'jane@gmail.com',
+//       name: 'Jane',
+//       avatarUrl: 'https://gravatar.com/...'
+//     }
+//   ],
+//   messages: [
+//     { id: '1', userId: '1', body: 'Hello', createdAt: Date.now() },
+//     { id: '2', userId: '2', body: 'Hi', createdAt: Date.now() },
+//     { id: '3', userId: '1', body: 'Hi Ho', createdAt: Date.now() }
+//   ]
+// };
 
-const typeDefs = gql`
-  type Query {
-    users: [User!]!
-    user(id: ID!): User
-    messages: [Message!]!
-  }
+// const typeDefs = gql`
+//   type Query {
+//     users: [User!]!
+//     user(id: ID!): User
+//     messages: [Message!]!
+//   }
 
-  type Mutation {
-    addUser(email: String!, name: String): User
-  }
+//   type Mutation {
+//     addUser(email: String!, name: String): User
+//   }
 
-  type User {
-    id: ID!
-    email: String!
-    name: String
-    avatarUrl: String
-    messages: [Message!]!
-  }
+//   type User {
+//     id: ID!
+//     email: String!
+//     name: String
+//     avatarUrl: String
+//     messages: [Message!]!
+//   }
 
-  type Message {
-    id: ID!
-    body: String!
-    createdAt: String
-  }
-`;
+//   type Message {
+//     id: ID!
+//     body: String!
+//     createdAt: String
+//   }
+// `;
 
-const resolvers = {
-  Query: {
-    users: () => db.users,
-    user: (root, { id }) => db.users.find(user => user.id === id),
-    messages: () => db.messages
-  },
-  Mutation: {
-    addUser: (root, { email, name }) => {
-      const user = {
-        id: crypto.randomBytes(10).toString('hex'),
-        email,
-        name
-      };
+// const resolvers = {
+//   Query: {
+//     users: () => db.users,
+//     user: (root, { id }) => db.users.find(user => user.id === id),
+//     messages: () => db.messages
+//   },
+//   Mutation: {
+//     addUser: (root, { email, name }) => {
+//       const user = {
+//         id: crypto.randomBytes(10).toString('hex'),
+//         email,
+//         name
+//       };
 
-      db.users.push(user);
+//       db.users.push(user);
 
-      return user;
-    }
-  },
-  User: {
-    messages: user => db.messages.filter(message => message.userId === user.id)
-  }
-};
+//       return user;
+//     }
+//   },
+//   User: {
+//     messages: user => db.messages.filter(message => message.userId === user.id)
+//   }
+// };
 
-const server = new ApolloServer({ typeDefs, resolvers });
+// const server = new ApolloServer({ typeDefs, resolvers });
 
-server.listen().then(({ url }) => console.log(`Server running at ${url}`));
+// server.listen().then(({ url }) => console.log(`Server running at ${url}`));
