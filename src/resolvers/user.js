d597ed34a7e1289249c0fdfab0b0ae65e28077fc
Paul Tobin <tobster111@gmail.com>
2018-12-02 10:21:19 +0000

    Added Resolvers

---
+++ src/resolvers/user.js
@@ -0,0 +1,15 @@
+export default {
+  Query: {
+    users: (root, args, context, info) => {
+      
+    },
+    user: (root, args, context, info) => {
+
+    }
+  },
+  Mutation: {
+    signUp: (root, args, context, info) => {
+
+    }
+  }
+}
