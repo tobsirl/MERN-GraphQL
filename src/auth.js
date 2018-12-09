6a6e3af0b4463434933d13c707b4f83be0a0ef3c
Paul Tobin <tobster111@gmail.com>
2018-12-09 11:20:23 +0000

    Added checkSignedIn function

--- src/auth.js
+++ src/auth.js
@@ -1,7 +1,11 @@
 import { AuthenticationError } from 'apollo-server-express';
 
-const checkSignedIn = req => {
+export const checkSignedIn = req => {
   if (req.session.userId) {
     throw new AuthenticationError('You must be signed in');
   }
 };
+
+const checkSignedOut = req => {
+  
+}
