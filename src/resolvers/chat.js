dd7bd6253ec0fa14ab3009d60bc72f921a2ac604
Paul Tobin <tobster111@gmail.com>
2019-02-06 10:55:27 +0000

    Finised the mutation for startChat

--- src/resolvers/chat.js
+++ src/resolvers/chat.js
@@ -1,7 +1,36 @@
+import Joi from 'joi';
+import { startChat } from '../schemas';
+import User from '../models/user';
+import { UserInputError } from 'apollo-server-core';
+
 export default {
   Mutation: {
-    startChat: (root, args, context, info) => {
-      Joi.validate
+    startChat: async (root, args, { req }, info) => {
+      const { userId } = req.session;
+      const { title, userIds } = args;
+
+      await Joi.validate(args, startChat(userId), { abortEarly: false });
+
+      const idsFound = await User.where('_id')
+        .in(userIds)
+        .countDocuments();
+
+      if (idsFound !== userIds.length) {
+        throw new UserInputError('One or more User IDs are invalid.');
+      }
+
+      userIds.push(userId);
+
+      const chat = chat.create({ title, users: userIds });
+
+      await User.updateMany(
+        { _id: { $in: userIds } },
+        {
+          $push: { chats: chat }
+        }
+      );
+
+      return chat;
     }
   }
 };
